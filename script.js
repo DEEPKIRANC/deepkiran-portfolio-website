2ec2cbddb68b14970a2c2dbc8bb5d59883e76520
Deep Kiran <cdeepkiran14@gmail.com>
2021-05-23 09:52:11 +0530

    Use localstorage to store css root variables
    Used this for persisting dark/light theme across multiple pages

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+window.onload=(event)=>{
+    const localObj=JSON.parse(window.localStorage.getItem("localObj"));
+    if(localObj!==null)
+    {
+        var r=document.querySelector(':root');
+        let elem=document.getElementById("themeChanger");
+        const {background__color:bg__color,text__color}=localObj;
+        r.style.setProperty('--lightModeColor',bg__color);
+        r.style.setProperty('--lightModeTextColor',text__color);
+
+        if(bg__color==="#192734")
+        {
+            
+            elem.classList.remove('fa-moon');
+            elem.classList.add('fa-sun');
+        }
+        
+
+    }
+};
+
+
 document.getElementById("themeChanger").addEventListener("click",()=>{
     var r=document.querySelector(':root');
     var current__background__color=getComputedStyle(r).getPropertyValue('--lightModeColor');
@@ -9,6 +31,11 @@ document.getElementById("themeChanger").addEventListener("click",()=>{
         setTimeout(()=>{
             r.style.setProperty('--lightModeColor','#192734');
             r.style.setProperty('--lightModeTextColor','#F3F4F6');
+            const localObj={
+                background__color:'#192734',
+                text__color:'#F3F4F6'
+            }
+            window.localStorage.setItem('localObj',JSON.stringify(localObj));
             elem.classList.remove('fa-moon');
             elem.classList.add('fa-sun');
         },100)
@@ -20,6 +47,11 @@ document.getElementById("themeChanger").addEventListener("click",()=>{
         setTimeout(()=>{
             r.style.setProperty('--lightModeColor','#F3F4F6');
             r.style.setProperty('--lightModeTextColor','#192734');
+            const localObj={
+                background__color:'#F3F4F6',
+                text__color:'#192734'
+            }
+            window.localStorage.setItem('localObj',JSON.stringify(localObj));
             elem.classList.remove('fa-sun');
             elem.classList.add('fa-moon');
         },100)
